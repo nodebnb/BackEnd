3584f544eb4fc6bf5f9d48b9999a9f00491f9626
Linghua Jin <ljin@walmartlabs.com>
2015-06-04 00:08:54 -0500

    fixed a async function trigger

--- bin/indexer.js
+++ bin/indexer.js
@@ -3,7 +3,7 @@
  * 1. run scrapper to generate bnb.json
  * 2. Import "bnb.json" to mongodb database "nodebnb" table "listings"
  * mongoimport --db nodebnb --collection listings --file bnb.json
-*/
+ */
 let elasticsearch = require('elasticsearch')
 let Listing = require('../app/models/listing')
 let mongoose = require('mongoose')
@@ -11,38 +11,44 @@ let mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/nodebnb')
 
 let client = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'trace'
+    host: 'localhost:9200',
+    log: 'trace'
 })
 const index = "listings"
 const type = "listing"
 
 require('songbird')
 
-async function indexListings() {
-    let result = await Listing.promise.find({})
-
+async
+function indexListings() {
+    let result
+    try {
+        result = await Listing.promise.find({})
+    } catch (e) {
+        console.log(">< error")
+    }
     let listings = result.reduce((items, item) => {
-        items.push({index: {_index: index, _type: type, _id: item._id}})
+        items.push({
+            index: {
+                _index: index,
+                _type: type,
+                _id: item._id
+            }
+        })
         items.push(item)
+        console.log(">< item")
         return items
     }, [])
 
-    await client.promise.bulk({body: listings})
+    await client.promise.bulk({
+        body: listings
+    })
 }
 
-(function() {
+(async function() {
     let start = Date.now()
-
     console.log("Starting Indexing @", start)
-
-    indexListings().catch(e => console.log(e.stack))
-
+    await indexListings().catch(e => console.log(e.stack))
     console.log("Succesfully indexed in", Date.now() - start, "ms");
-
     process.exit()
 }())
-
-
-
-
